712fd4ad6c9d8f8af09f6686f6c788839e32df9a
Sling-msg <arjunsaji09@gmail.com>
2026-04-10 15:56:28 +0530

    refactor: Update Android app theme and build config
    Removes splash screen and webview debugging from MainActivity. Updates Android app themes to use a no-action-bar style and a darker background. Adds `ic_launcher_background` to the color resources. Updates codemagic.yaml to ensure Capacitor syncs correctly.

--- android/app/src/main/java/com/sling/app/MainActivity.java
+++ android/app/src/main/java/com/sling/app/MainActivity.java
@@ -1,15 +1,11 @@
 package com.sling.app;
 
 import android.os.Bundle;
-import android.webkit.WebView;
-import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
-        WebView.setWebContentsDebuggingEnabled(true);
     }
 }
